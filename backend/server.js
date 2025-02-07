c14248e75fc19243b9310237afd7526e1bab725e
sujanchaudhary415 <sujanchaudhary415@gmail.com>
2025-02-07 09:32:05 +0545

    first commit

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import express from "express"
+import cors from "cors"
+import 'dotenv/config'
+
+
+//app config
+const app=express();
+const port=process.env.PORT || 3000;
+
+//middleware
+app.use(express.json());
+app.use(cors())
+
+//api endpoints
+app.get('/',(req,res)=>{
+    res.status(200).send("Api is running")
+})
+
+app.listen(port,()=>console.log(`Server is running on ${port}`))
